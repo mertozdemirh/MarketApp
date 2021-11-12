d9297c511a514bac6419e91a021d3e4379dda70e
mert özdemir <mertozdemir91@hotmail.com>
2021-11-12 22:55:58 +0300

    Crud islemleri baslangic

---
+++ MarketApp/Controllers/UrunlerController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MarketApp.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace MarketApp.Controllers
+{
+    public class UrunlerController : Controller
+    {
+        private readonly UygulamaDbContext _context;
+
+        public UrunlerController(UygulamaDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Urunler
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Urunler.ToListAsync());
+        }
+
+        // GET: Urunler/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var urun = await _context.Urunler
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (urun == null)
+            {
+                return NotFound();
+            }
+
+            return View(urun);
+        }
+
+        // GET: Urunler/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Urunler/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UrunAd,BirimFiyat,Resim")] Urun urun, IFormFile dosya)
+        {
+            if (dosya.Length > 10*1000*1000)
+            {
+                ModelState.AddModelError("Resim", "Görselin boyutu 10MB'dan büyük olamaz");
+            }
+            else if (!dosya.ContentType.StartsWith("image/"))
+            {
+                ModelState.AddModelError("Resim", "Bir resim dosyası seçmediniz");
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Add(urun);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(urun);
+        }
+
+        // GET: Urunler/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var urun = await _context.Urunler.FindAsync(id);
+            if (urun == null)
+            {
+                return NotFound();
+            }
+            return View(urun);
+        }
+
+        // POST: Urunler/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,UrunAd,BirimFiyat,Resim")] Urun urun)
+        {
+            if (id != urun.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(urun);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UrunExists(urun.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(urun);
+        }
+
+        // GET: Urunler/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var urun = await _context.Urunler
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (urun == null)
+            {
+                return NotFound();
+            }
+
+            return View(urun);
+        }
+
+        // POST: Urunler/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var urun = await _context.Urunler.FindAsync(id);
+            _context.Urunler.Remove(urun);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UrunExists(int id)
+        {
+            return _context.Urunler.Any(e => e.Id == id);
+        }
+    }
+}
